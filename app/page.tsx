4d87e883de28d4e9942f4bed4f55f9f985e2e6d2
Saim Khan <saimkhan065@gmail.com>
2024-07-24 17:33:25 -0400

    more components + models

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
+import connectDB from "@/config/database";
 
-const HomePage: React.FC = () => {
+const HomePage: React.FC = async () => {
+  //await connectDB();
   return (
     <>
       <Hero />
       <InfoBoxes />
       <div>Properties Home</div>
       <Link href="/properties">Show Properties</Link>
+      <HomeProperties />
     </>
   );
 };
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage: React.FC = () => {
   return (
     <>
-      <div>PropertiesPage</div>
-      <Link href="/">Go Home</Link>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          {/* <PropertySearchForm /> */}
+        </div>
+      </section>
+      {properties.map((prop) => (
+        <PropertyCard key={prop._id} property={prop} />
+      ))}
+      {/* <Properties /> */}
     </>
   );
 };
