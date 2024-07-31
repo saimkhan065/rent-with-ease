93f244b3dff34a35a26d85768b622bef76ccfff7
Saim Khan <saimkhan065@gmail.com>
2024-07-31 14:42:32 -0400

    minor bug fix

--- app/api/properties/search/route.jsx
+++ app/api/properties/search/route.jsx
@@ -2,7 +2,7 @@ import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
 // GET /api/properties/search
-export const GET = async (request: any) => {
+export const GET = async (request) => {
   try {
     await connectDB();
 
